403bdbf1c95f59cf57813b70f0b5a730630d77b4
khomenko <drotmen@gmail.com>
2019-04-15 19:22:44 +0300

    1.1122

---
+++ html/slider.js
@@ -0,0 +1,17 @@
+<script type="text/javascript">
+	jQuery( document ).ready(function( $ ) {
+		$( '#my-slider' ).sliderPro();
+	});
+</script>
+
+$( '#my-slider' ).sliderPro({
+	width: 400,
+	height: 400,
+	arrows: true,
+	buttons: false,
+	waitForLayers: true,
+	fade: true,
+	autoplay: false,
+    autoScaleLayers: false,
+    loop: true,
+});
