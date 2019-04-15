6169d921616502b0ebcdb3c9ccfde89082c0a24b
klvsov <klvsov@gmail.com>
2019-04-15 19:49:49 +0300

    add fucking slider

--- html/slider.js
+++ html/slider.js
@@ -1,17 +1,13 @@
-<script type="text/javascript">
-	jQuery( document ).ready(function( $ ) {
-		$( '#my-slider' ).sliderPro();
-	});
-</script>
-
-$( '#my-slider' ).sliderPro({
-	width: 400,
-	height: 400,
-	arrows: true,
-	buttons: false,
-	waitForLayers: true,
-	fade: true,
-	autoplay: false,
-    autoScaleLayers: false,
-    loop: true,
-});
+jQuery( document ).ready(function( $ ) {
+	$( '#my-slider' ).sliderPro({
+		width: 400,
+		height: 400,
+		arrows: true,
+		buttons: false,
+		waitForLayers: true,
+		fade: true,
+		autoplay: false,
+    	autoScaleLayers: false,
+    	loop: true,
+	})
+})
